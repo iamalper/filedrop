ac7f83e38a3bce062df12db6f5b64cb691efa93e
alper <29778028+iamalper@users.noreply.github.com>
2023-10-12 20:05:03 +0300

    test: create temp dir if not exists

--- test/fake_path_provider.dart
+++ test/fake_path_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:path/path.dart' as p;
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
@@ -7,8 +9,14 @@ class FakePathProviderPlatform extends Fake
     with MockPlatformInterfaceMixin
     implements PathProviderPlatform {
   @override
-  Future<String?> getTemporaryPath() async => p.absolute("tmp");
+  Future<String?> getTemporaryPath() async {
+    Directory(p.absolute("tmp")).createSync();
+    return p.absolute("tmp");
+  }
 
   @override
-  Future<String?> getDownloadsPath() async => p.absolute("tmp");
+  Future<String?> getDownloadsPath() async {
+    Directory(p.absolute("tmp")).createSync();
+    return p.absolute("tmp");
+  }
 }
