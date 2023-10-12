e198dfcd8abcbfedccaad141e53fb55022d3bc07
alper <29778028+iamalper@users.noreply.github.com>
2023-10-12 19:35:10 +0300

    build: fix temp dir for windows

--- test/fake_path_provider.dart
+++ test/fake_path_provider.dart
@@ -1,15 +1,14 @@
+import 'package:path/path.dart' as p;
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-const kTemporaryPath = '/tmp';
 
 class FakePathProviderPlatform extends Fake
     with MockPlatformInterfaceMixin
     implements PathProviderPlatform {
   @override
-  Future<String?> getTemporaryPath() async => kTemporaryPath;
+  Future<String?> getTemporaryPath() async => p.join("/tmp");
 
   @override
-  Future<String?> getDownloadsPath() async => kTemporaryPath;
+  Future<String?> getDownloadsPath() async => p.join("/tmp");
 }
